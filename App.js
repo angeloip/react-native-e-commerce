c2251962f8bb1d4a4b94c2b2e0d3d564f3093660
AngeloIngaJP <angelo.inga@unmsm.edu.pe>
2023-09-01 23:56:15 -0500

    app

--- App.js
+++ App.js
@@ -1,12 +1,40 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+import { useCallback } from 'react';
+import BottomTabNavigation from './src/navigation/BottomTabNavigation';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    regular: require('./assets/fonts/Poppins-Regular.ttf'),
+    bold: require('./assets/fonts/Poppins-Bold.ttf'),
+    semiBold: require('./assets/fonts/Poppins-SemiBold.ttf'),
+    medium: require('./assets/fonts/Poppins-Medium.ttf'),
+    light: require('./assets/fonts/Poppins-Light.ttf'),
+    extraBold: require('./assets/fonts/Poppins-ExtraBold.ttf'),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    await SplashScreen.hideAsync();
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  } 
+
+
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Botton Navigation" component={BottomTabNavigation} options={{headerShown: false}}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
@@ -17,4 +45,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  textStyle:{
+    fontFamily: 'regular',
+    fontSize: 20,
+    color: 'red'
+  }
 });
