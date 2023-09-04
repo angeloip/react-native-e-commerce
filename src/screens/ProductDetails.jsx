2a430d561f124f6769f71b830b789c3e55a418f6
AngeloIngaJP <angelo.inga@unmsm.edu.pe>
2023-09-04 13:37:01 -0500

    app

--- src/screens/ProductDetails.jsx
+++ src/screens/ProductDetails.jsx
@@ -1,8 +1,11 @@
-import { Text, View, TouchableOpacity } from 'react-native'
+import { Text, View, TouchableOpacity, Image } from 'react-native'
 import styles from '../styles/productDetails.style'
-import { Ionicons } from 'react-native-vector-icons'
+import { Ionicons, SimpleLineIcons } from 'react-native-vector-icons'
+import { useState } from 'react'
 
 const ProductDetails = ({ navigation }) => {
+  const [quantity, setQuantity] = useState(1)
+
   return (
     <View style={styles.container}>
       <View style={styles.upperRow}>
@@ -12,6 +15,50 @@ const ProductDetails = ({ navigation }) => {
         <TouchableOpacity onPress={() => {}}>
           <Ionicons name="heart" size={30} />
         </TouchableOpacity>
+      </View>
+      <Image
+        source={{
+          uri: 'https://i.ibb.co/SrtQZb3/cb2e64a8-ad4c-4d45-b58b-b0c7e11b6bb4-fn1.jpg'
+        }}
+        style={styles.image}
+      />
+      <View style={styles.details}>
+        <View style={styles.titleRow}>
+          <Text style={styles.title}>Hello my people</Text>
+          <View style={styles.priceWrapper}>
+            <Text style={styles.price}>$ 660.00</Text>
+          </View>
+        </View>
+
+        <View style={styles.ratingRow}>
+          <View style={styles.rating}>
+            {[...Array(5)].map((_, i) => (
+              <Ionicons key={i} name="star" size={24} color="gold" />
+            ))}
+
+            <Text style={styles.ratingTxt}> (4.9)</Text>
+          </View>
+
+          <View style={styles.quantity}>
+            <TouchableOpacity
+              onPress={() => setQuantity(quantity - 1)}
+              disabled={quantity <= 1}
+            >
+              <SimpleLineIcons name="minus" size={24} />
+            </TouchableOpacity>
+
+            <Text style={styles.quantityTxt}>{quantity}</Text>
+
+            <TouchableOpacity
+              onPress={() => setQuantity(quantity + 1)}
+              disabled={quantity >= 9}
+            >
+              <SimpleLineIcons name="plus" size={24} />
+            </TouchableOpacity>
+          </View>
+        </View>
+
+        <View style={styles.descriptionWrapper}></View>
       </View>
     </View>
   )
--- src/styles/productDetails.style.js
+++ src/styles/productDetails.style.js
@@ -3,7 +3,8 @@ import { COLORS, SIZES } from '../constants'
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1
+    flex: 1,
+    backgroundColor: COLORS.lightWhite
   },
   upperRow: {
     marginHorizontal: 20,
@@ -14,6 +15,66 @@ const styles = StyleSheet.create({
     top: SIZES.xxLarge,
     width: SIZES.width - 44,
     zIndex: 100
+  },
+  image: {
+    aspectRatio: 1,
+    resizeMode: 'cover'
+  },
+  details: {
+    marginTop: -SIZES.large,
+    backgroundColor: COLORS.lightWhite,
+    borderTopLeftRadius: SIZES.medium,
+    borderTopRightRadius: SIZES.medium
+  },
+  titleRow: {
+    marginHorizontal: SIZES.large,
+    paddingBottom: SIZES.small,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    top: 20
+  },
+  title: {
+    fontFamily: 'bold',
+    fontSize: SIZES.large
+  },
+  priceWrapper: {
+    backgroundColor: COLORS.secondary,
+    borderRadius: SIZES.large
+  },
+  price: {
+    fontFamily: 'semiBold',
+    fontSize: SIZES.large,
+    paddingHorizontal: 10
+  },
+  ratingRow: {
+    paddingBottom: SIZES.small,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginTop: 5,
+    marginHorizontal: SIZES.large
+  },
+  rating: {
+    top: SIZES.large,
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    alignItems: 'center'
+  },
+  ratingTxt: {
+    fontFamily: 'medium',
+    color: COLORS.gray
+  },
+  quantity: {
+    top: SIZES.large,
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 10
+  },
+  quantityTxt: {
+    fontFamily: 'medium',
+    color: COLORS.gray,
+    fontSize: SIZES.medium
   }
 })
 
